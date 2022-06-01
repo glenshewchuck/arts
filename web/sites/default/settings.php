1dfc12ba90f923dde5290a7b885c8b8d05df8dd2
Glen on Lightsail <glenshewchuck@gmail.com>
2022-06-01 17:34:58 +0000

    most recent stuff including sync dir

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -766,7 +766,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
+$settings['config_sync_directory'] = 'sites/default/files/config_8JLcdYgIERu-6i00nmxIn3mPpOp8dBGTcmR6pFYRf20raBoVx9P34SdDuDav51bX96cJlOMWmg/sync';
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
-$settings['config_sync_directory'] = 'sites/default/files/config_8JLcdYgIERu-6i00nmxIn3mPpOp8dBGTcmR6pFYRf20raBoVx9P34SdDuDav51bX96cJlOMWmg/sync';
